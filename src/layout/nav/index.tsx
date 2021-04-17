5a19c6a77d0d9d049f22da91ce14fb5f0616b442
houfei <fei.hou@wenba100.com>
2021-04-17 23:04:57 +0800

    将 Nav 组件改为 hooks

--- src/layout/nav/index.tsx
+++ src/layout/nav/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Layout, Menu, SubMenuProps, MenuItemProps } from 'antd';
 import { withRouter, Link } from 'react-router-dom';
 import { RouteComponentProps } from 'react-router';
@@ -8,106 +8,40 @@ import './index.scss';
 const { Sider } = Layout;
 const { SubMenu } = Menu;
 
-type IProps = {
-  collapsed: boolean;
-} & RouteComponentProps;
-
-type IState = {
-  openKey: string;
-  menuNodes: (SubMenuProps & MenuItemProps) | null;
-};
-
-interface IMenuList {
+interface IRoutes {
   title: string;
   key: string;
   icon: string;
-  isPublic?: boolean;
-  children?: IMenuList[];
+  id: number;
+  pid: number;
+  childen?: IRoutes;
 }
 
-const menuList: IMenuList[] = [
-  {
-    title: '首页',
-    key: '/dashboard',
-    icon: 'VideoCameraOutlined',
-    isPublic: true
-  },
-  {
-    title: '商品',
-    key: '/products',
-    icon: 'UploadOutlined',
-    children: [
-      {
-        title: '品类管理',
-        key: '/category',
-        icon: 'DesktopOutlined'
-      },
-      {
-        title: '商品管理',
-        key: '/product',
-        icon: 'PieChartOutlined'
-      }
-    ]
-  },
+type IProps = {
+  collapsed: boolean;
+  openKey: string[];
+  routes: IRoutes[];
+} & RouteComponentProps;
 
-  {
-    title: '用户管理',
-    key: '/user',
-    icon: 'FileOutlined'
-  },
-  {
-    title: '角色管理',
-    key: '/role',
-    icon: 'TeamOutlined'
-  },
+const Nav = (props: IProps) => {
+  const [openKey, setOpenKey] = useState<string[]>([]);
+  const [menuNodes, setMenuNodes] = useState<
+    (SubMenuProps & MenuItemProps) | null
+  >(null);
 
-  {
-    title: '图形图表',
-    key: '/charts',
-    icon: 'UserOutlined',
-    children: [
-      {
-        title: '柱形图',
-        key: '/charts/bar',
-        icon: 'UploadOutlined'
-      },
-      {
-        title: '折线图',
-        key: '/charts/line',
-        icon: 'DesktopOutlined'
-      },
-      {
-        title: '饼图',
-        key: '/charts/pie',
-        icon: 'PieChartOutlined'
-      }
-    ]
-  },
-  {
-    title: '状态管理',
-    key: '/store',
-    icon: 'FileOutlined'
-  }
-];
+  useEffect(() => {
+    setMenuNodes(getMenuNodes(props.routes));
+  }, [props.routes]);
 
-class Nav extends React.Component<IProps, IState> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      openKey: '/app/dashboard',
-      menuNodes: null
-    };
-  }
-  getIcon = (iconname: string) => React.createElement(Icon[iconname]);
+  const getIcon = (iconname: string) => React.createElement(Icon[iconname]);
 
-  getMenuNodes = (menuList) => {
-    const path = this.props.location.pathname;
+  const getMenuNodes = (menuList) => {
+    const path = props.location.pathname;
     return menuList.reduce((pre, item) => {
       if (!item.children) {
-        const key = `/app${item.key}`;
         pre.push(
-          <Menu.Item key={key} icon={this.getIcon(item.icon)}>
-            <Link to={key}>
+          <Menu.Item key={item.key} icon={getIcon(item.icon)}>
+            <Link to={item.key}>
               <span>{item.title}</span>
             </Link>
           </Menu.Item>
@@ -115,19 +49,15 @@ class Nav extends React.Component<IProps, IState> {
       } else {
         // 查找一个与当前请求路径匹配的子Item
         const cItem = item.children.find((cItem) => {
-          return path.indexOf(`/app${cItem.key}`) === 0;
+          return path.indexOf(cItem.key) === 0;
         });
-        // 如果存在, 说明当前item的子列表需要打开
+        // 如果存在, 说明当前item的子列表需要打开 TODO bug
         if (cItem) {
-          const key = `/app${item.key}`;
-          this.setState({
-            openKey: key
-          });
+          setOpenKey(item.key);
         }
-        const key = `/app${item.key}`;
         pre.push(
-          <SubMenu key={key} icon={this.getIcon(item.icon)} title={item.title}>
-            {this.getMenuNodes(item.children)}
+          <SubMenu key={item.key} icon={getIcon(item.icon)} title={item.title}>
+            {getMenuNodes(item.children)}
           </SubMenu>
         );
       }
@@ -136,50 +66,37 @@ class Nav extends React.Component<IProps, IState> {
     }, []);
   };
 
-  /*
-  在第一次render()之前执行一次
-  为第一个render()准备数据(必须同步的)
-   */
-  componentWillMount() {
-    this.setState({
-      menuNodes: this.getMenuNodes(menuList)
-    });
+  const { collapsed } = props;
+  let path = props.location.pathname;
+  // 如果访问 /products'，想要自动跳转到默认子路由
+  if (path.indexOf('/products') === 0) {
+    path = '/product';
   }
 
-  render() {
-    const { collapsed } = this.props;
-    const { openKey, menuNodes } = this.state;
-    const path = this.props.location.pathname;
-    // 如果访问 /app/products'，想要自动跳转到默认子路由
-    // if (path.indexOf('/app/products') === 0) {
-    //   path = '/app/product';
-    // }
-
-    return (
-      <Sider
-        style={{
-          overflow: 'auto',
-          height: '100vh',
-          position: 'fixed',
-          left: 0
-        }}
-        breakpoint="lg"
-        trigger={null}
-        collapsible
-        collapsed={collapsed}
+  return (
+    <Sider
+      style={{
+        overflow: 'auto',
+        height: '100vh',
+        position: 'fixed',
+        left: 0
+      }}
+      breakpoint="lg"
+      trigger={null}
+      collapsible
+      collapsed={collapsed}
+    >
+      <div className="logo" />
+      <Menu
+        mode="inline"
+        theme="dark"
+        selectedKeys={[path]}
+        defaultOpenKeys={openKey}
       >
-        <div className="logo" />
-        <Menu
-          mode="inline"
-          theme="dark"
-          selectedKeys={[path]}
-          defaultOpenKeys={[openKey]}
-        >
-          {menuNodes}
-        </Menu>
-      </Sider>
-    );
-  }
-}
+        {menuNodes}
+      </Menu>
+    </Sider>
+  );
+};
 
 export default withRouter(Nav);
