4037e3ca9b9021de6c01c7527e1f41cc0defe1c3
houfei <fei.hou@wenba100.com>
2021-04-21 23:55:25 +0800

    在header增加menu菜单

--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -1,21 +1,51 @@
 import React from 'react';
-import { Layout } from 'antd';
-import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
+import { Layout, Menu, Badge } from 'antd';
+import screenfull from 'screenfull';
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  NotificationOutlined,
+  FullscreenOutlined,
+  FullscreenExitOutlined,
+  SettingOutlined
+} from '@ant-design/icons';
 import MyCenter from '../../components/MyCenter';
 import './index.scss';
 
+const { SubMenu } = Menu;
 interface IProps {
   collapsed: boolean;
   toggle: () => void;
 }
 
-class Header extends React.Component<IProps> {
+interface IState {
+  isFullscreen: boolean;
+}
+
+class Header extends React.Component<IProps, IState> {
+  state = {
+    isFullscreen: false
+  };
+  handleClick = (e) => {
+    console.log('click ', e);
+    if (e.key === 'screenfull') {
+      if (screenfull.isEnabled) {
+        const { isFullscreen } = this.state;
+        this.setState({
+          isFullscreen: !isFullscreen
+        });
+        screenfull.toggle();
+      }
+    }
+  };
   render() {
     const { collapsed, toggle } = this.props;
+    const { isFullscreen } = this.state;
     return (
       <Layout.Header
         style={{
           backgroundColor: 'white',
+          height: '66px',
           padding: '0',
           overflow: 'auto',
           width: '100%',
@@ -31,7 +61,44 @@ class Header extends React.Component<IProps> {
           }
         )}
         <div className="header-userinfo">
-          <MyCenter></MyCenter>
+          <Menu onClick={this.handleClick} mode="horizontal">
+            <Menu.Item
+              key="screenfull"
+              icon={
+                isFullscreen ? (
+                  <FullscreenExitOutlined />
+                ) : (
+                  <FullscreenOutlined />
+                )
+              }
+            >
+              全屏
+            </Menu.Item>
+            <Menu.Item key="messgae" icon={<NotificationOutlined />}>
+              <Badge dot>
+                <span
+                  style={{
+                    marginRight: '6px'
+                  }}
+                >
+                  消息
+                </span>
+              </Badge>
+            </Menu.Item>
+            <SubMenu key="SubMenu" icon={<SettingOutlined />} title="系统">
+              <Menu.ItemGroup title="Item 1">
+                <Menu.Item key="setting:1">Option 1</Menu.Item>
+                <Menu.Item key="setting:2">Option 2</Menu.Item>
+              </Menu.ItemGroup>
+              <Menu.ItemGroup title="Item 2">
+                <Menu.Item key="setting:3">Option 3</Menu.Item>
+                <Menu.Item key="setting:4">Option 4</Menu.Item>
+              </Menu.ItemGroup>
+            </SubMenu>
+            <Menu.Item key="my-center">
+              <MyCenter></MyCenter>
+            </Menu.Item>
+          </Menu>
         </div>
       </Layout.Header>
     );
