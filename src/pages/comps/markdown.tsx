d9d23a307540d0ef24bbd9de07a27fbc91d51410
候飞 <houfei@pwrd.com>
2021-11-29 19:13:44 +0800

    测试 md

--- src/pages/comps/markdown.tsx
+++ src/pages/comps/markdown.tsx
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
 
-class Markdown extends Component {
-  render() {
-    return (
-      <div
-        style={{ border: '2px solid red', padding: 20, textAlign: 'center' }}
-      >
-        这是 Markdown 页面
-      </div>
-    );
-  }
-}
+const Markdown = () => {
+  const markdown = `<div class="note">
+
+  Some *emphasis* and <strong>strong</strong>!
+  
+  </div>`;
+  return (
+    <div style={{ border: '2px solid red', padding: 20, textAlign: 'center' }}>
+      <ReactMarkdown
+        children={markdown}
+        remarkPlugins={[[remarkGfm, rehypeRaw]]}
+      ></ReactMarkdown>
+    </div>
+  );
+};
 
 export default Markdown;
