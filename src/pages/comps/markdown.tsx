04d2f44736b079562633b1cb32b6c9bb8168f4ef
候飞 <houfei@pwrd.com>
2021-11-30 18:47:09 +0800

    添加文件

--- src/pages/comps/markdown.tsx
+++ src/pages/comps/markdown.tsx
@@ -1,22 +1,55 @@
-import React from 'react';
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-import rehypeRaw from 'rehype-raw';
+import React, { useMemo, useState } from 'react';
+
+// md 编辑器
+import { createEditor } from 'slate';
+import { Slate, Editable, withReact } from 'slate-react';
+
+// import ReactMarkdown from 'react-markdown';
+// import remarkGfm from 'remark-gfm';
+// import rehypeRaw from 'rehype-raw';
 
 const Markdown = () => {
-  const markdown = `<div class="note">
-
-  Some *emphasis* and <strong>strong</strong>!
-  
-  </div>`;
+  // 跟踪编辑器中 value 的值.
+  const [value, setValue] = useState([
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of text in a paragraph.' }]
+    }
+  ]);
+  // 创建一个不会在渲染中变化的 Slate 编辑器对象。
+  const editor = useMemo(() => withReact<any>(createEditor()), []);
   return (
-    <div style={{ border: '2px solid red', padding: 20, textAlign: 'center' }}>
-      <ReactMarkdown
-        children={markdown}
-        remarkPlugins={[[remarkGfm, rehypeRaw]]}
-      ></ReactMarkdown>
+    <div style={{ border: '2px solid red', padding: 20 }}>
+      <Slate
+        editor={editor}
+        value={value}
+        onChange={(newValue: any) => setValue(newValue)}
+      >
+        <Editable />
+      </Slate>
     </div>
   );
 };
 
 export default Markdown;
+
+// const Markdown = () => {
+//   const markdown = `<div class="note">
+//   Some *emphasis* and <strong>strong</strong>!
+//   <img src="https://octodex.github.com/images/minion.png" alt="image-20211129101037662" style="zoom:50%;" />
+//   ![Minion](https://octodex.github.com/images/minion.png)
+//   </div>`;
+//   return (
+//     <div style={{ border: '2px solid red', padding: 20, textAlign: 'center' }}>
+//       <ReactMarkdown
+//         children={markdown}
+//         remarkPlugins={[[remarkGfm]]}
+//         rehypePlugins={[[rehypeRaw]]}
+//         skipHtml={true}
+//         sourcePos={true}
+//       ></ReactMarkdown>
+//     </div>
+//   );
+// };
+
+// export default Markdown;
