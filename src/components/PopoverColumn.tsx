898de3f2435317cf8438101d14dddf2c2a7f9052
houfei1995@gmail.com <houfei1995@gmail.com>
2021-12-11 21:08:37 +0800

    增加显示项组件

--- src/components/PopoverColumn.tsx
+++ src/components/PopoverColumn.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { Table, Button, Checkbox, Popover, List } from 'antd';
+import { Table, Button, Popover, message } from 'antd';
 
-const PopoverColumn = ({ configColumns, columns }) => {
+const PopoverColumn = ({ configColumns, columns, update }) => {
   const [selected, setSelected] = useState<string[]>(
     columns.map((col) => col.key)
   );
@@ -9,29 +9,53 @@ const PopoverColumn = ({ configColumns, columns }) => {
     key: col.key,
     title: col.title
   }));
+  const saveColumns = () => {
+    if (selected.length === 0) {
+      return message.error('至少展示一列');
+    }
+    const result: any = [];
+    configColumns.forEach((row) => {
+      selected.forEach((select) => {
+        if (row.key === select) {
+          result.push(row);
+        }
+      });
+    });
+    update(result);
+  };
   const content = (
-    <Table
-      style={{ width: 160 }}
-      rowKey={(row) => row.key}
-      size="small"
-      columns={[
-        {
-          title: '列',
-          dataIndex: 'title',
-          key: 'title'
-        }
-      ]}
-      rowSelection={{
-        type: 'checkbox',
-        selectedRowKeys: selected,
-        onChange: (selectedRowKeys, selectedRows) => {
-          setSelected(selectedRowKeys as string[]);
-        }
-      }}
-      pagination={false}
-      dataSource={baseCol}
-      scroll={{ x: 'max-content', y: 200 }}
-    />
+    <>
+      <Table
+        style={{ width: 200 }}
+        rowKey={(row) => row.key}
+        size="small"
+        columns={[
+          {
+            title: '显示列',
+            dataIndex: 'title',
+            key: 'title'
+          }
+        ]}
+        rowSelection={{
+          type: 'checkbox',
+          selectedRowKeys: selected,
+          onChange: (selectedRowKeys) => {
+            setSelected(selectedRowKeys as string[]);
+          }
+        }}
+        pagination={false}
+        dataSource={baseCol}
+        scroll={{ x: 'max-content', y: 200 }}
+      />
+      <Button
+        type="primary"
+        size="small"
+        style={{ display: 'block', margin: '10px auto 0' }}
+        onClick={saveColumns}
+      >
+        保存
+      </Button>
+    </>
   );
   return (
     <Popover content={content} placement="bottom">
